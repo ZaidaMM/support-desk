16b8ed9394eb340092ac8563d2ad535b16b1396a
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-23 11:55:41 +0100

    error middleware, express async handler setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,25 @@
-const registerUser = (req, res) => {
-  res.send('Register Route');
-};
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
+// description: Register a new user
+// route: /api/users
+// access: Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  res.send('Register Route');
+});
+
+// description: Login user
+// route: /api/users/login
+// access: Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route');
-};
+});
 
 module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(201).json({ message: 'HOLA' });
@@ -11,5 +14,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
