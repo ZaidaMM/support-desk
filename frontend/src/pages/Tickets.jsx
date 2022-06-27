915e0ffc5d8ae0462b075fc35332ea3826c676b8
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-27 22:03:52 +0100

    ticket list

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 import Spinner from '../components/Spinner';
 import BackButton from '../components/BackButton';
+import TicketItem from '../components/TicketItem';
 
 const Tickets = () => {
   const { isLoading, tickets, isSuccess } = useSelector(
@@ -28,10 +29,21 @@ const Tickets = () => {
   }
 
   return (
-    <div>
+    <>
       <BackButton url='/' />
       <h1>Tickets</h1>
-    </div>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   );
 };
 export default Tickets;
